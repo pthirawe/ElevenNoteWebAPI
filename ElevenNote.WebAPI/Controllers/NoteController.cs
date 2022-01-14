dfc794417b6dbf3b001d56c5596753b633294042
Patom Thirawetchayan <patomt@gmail.com>
2022-01-14 09:37:53 -0500

    added get method to NoteController

--- ElevenNote.WebAPI/Controllers/NoteController.cs
+++ ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -27,6 +27,13 @@ namespace ElevenNote.WebAPI.Controllers
             return Ok(notes);
         }
 
+        public IHttpActionResult Get(int id)
+        {
+            NoteService noteService = CreateNoteService();
+            var note = noteService.GetNoteById(id);
+            return Ok(note);
+        }
+
         public IHttpActionResult Post(NoteCreate note)
         {
             if (!ModelState.IsValid)
